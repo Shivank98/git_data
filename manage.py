96afab5d06e0b3e04e910c4eccc513a104f3e3c7
megha9998 <33155241+megha9998@users.noreply.github.com>
2017-10-27 18:51:42 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
+        # adding comment
         try:
             import django
         except ImportError:
